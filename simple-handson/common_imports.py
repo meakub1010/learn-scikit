84bbf7cee59310caaac41ffa8899d49c2b7b7a0d
Muhammad Eakub <meakub86@gmail.com>
2025-10-07 01:38:28 -0400

    updated scikit boston housing

---
+++ simple-handson/common_imports.py
@@ -0,0 +1,13 @@
+# common_imports.py
+
+# Data libraries
+import numpy as np
+import pandas as pd
+
+# Plotting
+import matplotlib.pyplot as plt
+
+# Machine Learning
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
